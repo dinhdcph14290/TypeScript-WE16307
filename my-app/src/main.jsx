fea622767321346632ca6ff95164a7c040377935
CongDinh <dinhdcph14290@fpt.edu.vn>
2022-03-16 13:01:34 +0700

    session 2

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+// const myName = "Dong Cong Dinh";
+// const myAge = 20;
+// const myStatus = true;
+// const person = {
+//   name : "Phuong Anh",
+//   age : 20
+// }
+// const showInfo = (user) => {
+//   return <p>Thong tin user</p>
+// }
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // <div>
+  //   <h1>Hello {myName}</h1>
+  //   <h2>Age:{myAge}</h2>
+  //   <h3>{myStatus ? "Da ket hon" : "Chua ket hon"}</h3>
+  //   {showInfo(person)}
+  // </div>
+  <App />,document.getElementById('root')
 )
